2e08bea3c0ecf1690cc12f4da11e3ad7a0ac8652
clever88 <prostopavel2020@yandex.ru>
2023-10-16 16:13:37 +0800

    add bd

--- applications/settings.py
+++ applications/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'jazzmin',
     'core',
     'main',
+    'request',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- core/models.py
+++ core/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+
+
+class office(models.Model):
+    number = models.CharField('Номера кабинета', default="", max_length=9)
+
+    class Meta:
+        #managed = False
+        db_table = 'office'
+        verbose_name = 'кабинет'
+        verbose_name_plural = 'кабинеты'
+
+    def __str__(self):
+        return f'{self.number}'
+    
+
+class application(models.Model):
+    auth_user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, null=True, blank=True)
+    car = models.ForeignKey(office, on_delete=models.CASCADE, null=True, blank=True, verbose_name = 'Выберите кабинет')
+    date = models.DateTimeField('Дата записи')
+    description = models.TextField('Описание проблемы')
+
+    class Meta:
+        #managed = False
+        db_table = 'application'
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
+    def __str__(self):
+        return f'{self.date}'
+    
+
